cf42239039f5d7ce76893d024dd7fe3821c151f8
Marcelo dos Santos <mdssjc@gmail.com>
2021-10-07 22:15:40 -0300

    feat: add filter by name

--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { PostService } from '../service/post.service';
 import { Post } from '../model/Post';
+import { PostService } from '../service/post.service';
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.css'],
 })
 export class FeedComponent implements OnInit {
-
   listPost: Post[];
-  post: Post = new Post;
+  post: Post = new Post();
   nome: string;
+  filterName: string = '';
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-    this.findPosts()
+    this.findPosts();
   }
 
-  findPosts() {
+  findPosts(): void {
     this.postService.getPosts().subscribe((data: Post[]) => {
-      this.listPost = data;
-    })
+      this.listPost = data.filter(
+        (post) =>
+          this.filterName === '' ||
+          post.nome.toLowerCase().includes(this.filterName.toLowerCase())
+      );
+    });
   }
 
-  cadastrarMensagem() {
+  cadastrarMensagem(): void {
     this.postService.postMensagem(this.post).subscribe((data: Post) => {
-      this.post = data
-      location.assign('/feed')
-    })
+      this.post = data;
+      location.assign('/feed');
+    });
   }
 
+  filter(): void {
+    this.findPosts();
+  }
 }
